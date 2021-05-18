e6534164b4d4f98cf84cd5e4156f8e5d3433bfa7
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-05-18 10:38:16 +0000

    Update tk.py

--- tk.py
+++ tk.py
@@ -4,7 +4,7 @@ from functools import partial
 from tkinter import messagebox
 
 
-win = Tk()
+#win = Tk()
 #Intro
 '''
 def pr():
@@ -267,3 +267,180 @@ circle(30,0,1,'circle')
 ti.sleep(2)
 t.hideturtle()
 '''
+#Snake game
+
+'''
+import turtle as t
+import random as rd
+
+t.bgcolor('purple')
+
+caterpillar = t.Turtle()
+caterpillar.shape('square')
+caterpillar.speed(0)  
+caterpillar.penup()
+caterpillar.hideturtle()
+
+leaf = t.Turtle()
+leaf_shape = ((0,0),(14,2),(18,6),(20,20),(6,18),(2,14))
+t.register_shape('leaf',leaf_shape)
+leaf.shape('leaf')
+leaf.color('green')
+leaf.penup()
+leaf.hideturtle()
+leaf.speed()
+
+game_started = False
+text_turtle = t.Turtle()
+text_turtle.write('Press SPACE to start',align='center',font=('Arial',16,'bold'))
+text_turtle.hideturtle()
+
+score_turtle = t.Turtle()
+score_turtle.hideturtle()
+score_turtle.speed(0)
+
+def outside_window():
+    left_wall = -t.window_width()/2
+    right_wall = t.window_width()/2
+    top_wall = t.window_height()/2
+    bottom_wall = -t.window_height()/2
+    (x,y) = caterpillar.pos()
+    outside = x < left_wall or  x > right_wall or  y < bottom_wall or y > top_wall
+    return outside
+
+def game_over():
+    caterpillar.color('yellow')
+    leaf.color('yellow')
+    t.penup()
+    t.hideturtle()
+    t.write('GAME OVER!',align='center' , font=('Aerial',30,'normal'))
+
+def display_score(current_score):
+    score_turtle.clear()
+    score_turtle.penup()
+    x = (t.window_width() / 2)-50
+    y = (t.window_height() / 2)-50
+    score_turtle.setpos(x,y)
+    score_turtle.write(str(current_score) , align = 'right',font=('Arial',40,'bold'))
+
+def place_leaf():
+    leaf.hideturtle()
+    leaf.setx(rd.randint(-200,200))
+    leaf.sety(rd.randint(-200,200))
+    leaf.showturtle()
+
+def start_game():
+    global game_started
+    if game_started:
+        return
+    game_started = True
+
+    score = 0
+    text_turtle.clear()
+
+    caterpillar_speed = 2
+    caterpillar_length = 3
+    caterpillar.shapesize(1,caterpillar_length,1)
+    caterpillar.showturtle()
+    display_score(score)
+    place_leaf()
+
+    while True:
+        caterpillar.forward(caterpillar_speed)
+        if caterpillar.distance(leaf)<20:
+            place_leaf()
+            caterpillar_length = caterpillar_length + 1
+            caterpillar.shapesize(1,caterpillar_length,1)
+            caterpillar_speed = caterpillar_speed + 1
+            score = score + 10
+            display_score(score)
+        if outside_window():
+            game_over()
+            break
+
+def move_up():
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(90)
+
+def move_down():
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(270)
+
+def move_left():
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(180)
+
+def move_right():
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(0)
+
+t.onkey(start_game,'space')
+t.onkey(move_up,'Up')
+t.onkey(move_right,'Right')
+t.onkey(move_down,'Down')
+t.onkey(move_left,'Left')
+t.listen()
+t.mainloop()
+'''
+
+#matchmaker
+
+'''
+import random
+import time
+from tkinter import Tk,Button,DISABLED
+
+def show_symbol(x,y):
+    global first
+    global prevx,prevy
+    buttons[x,y]['text']=button_symbols[x,y]
+    buttons[x,y].update_idletasks()
+      
+    if first:
+        prevx=x
+        prevy=y 
+        first=False
+    elif prevx != x or prevy != y:
+        if buttons[prevx,prevy]['text'] != buttons [x,y] ['text']:
+            time.sleep(0.5)
+            buttons[prevx,prevy]['text'] = ' '
+            buttons[x,y]['text']=' '
+        else:
+            buttons[prevx,prevy]['command']= DISABLED
+            buttons[x,y]['command']= DISABLED
+        first= True     
+
+
+win = Tk()
+win.title('Matchmaker')
+win.resizable(width=False,height=False)
+first=True
+prevx=0
+prevy=0
+
+buttons={}
+button_symbols = {}
+symbols =[u'\u2702',u'\u2705',u'\u2708',u'\u2709',u'\u270A',u'\u270B',
+          u'\u270C',u'\u270F',u'\u2712',u'\u2714',u'\u2716',u'\u2728',
+          u'\u2702',u'\u2705',u'\u2708',u'\u2709',u'\u270A',u'\u270B',
+          u'\u270C',u'\u270F',u'\u2712',u'\u2714',u'\u2716',u'\u2728']
+
+random.shuffle(symbols)          
+
+for x in range(6):
+    for y in range(4):
+        button= Button(command = lambda x=x , y=y: show_symbol(x,y),width=10, height=8)
+        button.grid(column=x,row=y)
+        buttons[x,y]=button
+        button_symbols[x,y]=symbols.pop()
+
+print(u'\u270B')
+print(u'\u270C')
+print(u'\u2705')
+
+win.mainloop()
+'''
+
+#smart calculator 
+
+
