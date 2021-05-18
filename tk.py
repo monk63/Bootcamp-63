779fdd0ca57d78929ba66137fe5f6224debf369d
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-05-18 20:30:58 +0000

    Update tk.py

--- tk.py
+++ tk.py
@@ -442,5 +442,200 @@ win.mainloop()
 '''
 
 #smart calculator 
+'''
+from tkinter import *
 
+def add(a,b):
+    return a + b
 
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
+
+def mod(a,b):
+    return a % b
+
+def lcm(a,b):
+    L = a if a>b else b
+    while L <= a*b:
+        if L%a == 0 and L%b == 0:
+            return L
+        L+=1
+
+def hcf(a,b):
+    H = a if a<b else b
+    while H >= 1:
+        if a%H == 0 and b%H == 0:
+            return H
+        H-=1
+
+def extraxt_from_text(text):
+    l = []
+    for t in text.split(' '):
+        try:
+            l.append(float(t))
+        except ValueError:
+            pass
+    return l
+
+def calculate():
+    text = textin.get()
+    for word in text.split(' '):
+        if word.upper() in operations.keys():
+            try:
+                l = extraxt_from_text(text)
+                r = operations[word.upper()](l[0] , l[1])
+                list.delete(0,END)
+                list.insert(END,r)
+            except:
+                list.delete(0,END)
+                list.insert(END,'something went wrong please enter again')
+            finally:
+                break
+        elif word.upper() not in operations.keys():
+            list.delete(0,END)
+            list.insert(END,'something went wrong please enter again')
+
+operations = {'ADD':add , 'ADDITION':add , 'SUM':add , 'PLUS':add ,
+                'SUB':sub , 'DIFFERENCE':sub , 'MINUS':sub , 'SUBTRACT':sub,
+                 'LCM':lcm , 'HCF':hcf , 'PRODUCT':mul , 'MULTIPLICATION':mul,
+                 'MULTIPLY':mul , 'DIVISION':div , 'DIV':div ,'DIVIDE':div, 'MOD':mod ,
+                  'REMANDER':mod , 'MODULUS':mod}
+
+win = Tk()
+win.geometry('500x300')
+win.title('Smart Pugger')
+win.configure(bg='red')
+
+l1 = Label(win , text='I am a smart calculator',width=20 , padx=3)
+l1.place(x=150,y=10)
+l2 = Label(win , text='My name is Elon Mars' , padx=3)
+l2.place(x=180,y=40)
+l3 = Label(win , text='What can I help you' , padx=3)
+l3.place(x=176,y=130)
+
+textin = StringVar()
+e1 = Entry(win , width=30 , textvariable = textin)
+e1.place(x=100,y=160)
+
+b1 = Button(win , text='Just this' ,command=calculate)
+b1.place(x=210,y=200)
+
+list = Listbox(win,width=20,height=3)
+list.place(x=150,y=230)
+
+win.mainloop()
+'''
+
+# database
+from tkinter import *
+import backend
+
+def get_selected_row(event):
+    global selected_row
+    index = list.curselection()[0]
+    selected_row = list.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_row[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_row[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_row[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_row[4])
+    e5.delete(0,END)
+    e5.insert(END,selected_row[5])
+    e6.delete(0,END)
+    e6.insert(END,selected_row[6])
+
+def delete_command():
+    backend.delete(selected_row[0])
+
+def view_command():
+    list.delete(0,END)
+    for row in backend.view():
+        list.insert(END,row)
+
+def search_command():
+    list.delete(0,END)
+    for row in backend.search(date_text.get(),earning_text.get(),exercise_text.get(),study_text.get(),diet_text.get(),python_text.get()):
+        list.insert(END,row)
+
+def add_command():
+    backend.insert(date_text.get(),earning_text.get(),exercise_text.get(),study_text.get(),diet_text.get(),python_text.get())
+
+    list.delete(0,END)
+    list.insert(END,(date_text.get(),earning_text.get(),exercise_text.get(),study_text.get(),diet_text.get(),python_text.get()))
+
+win = Tk()
+
+win.wm_title('MY ROUTINE DATABASE')
+
+l1 = Label(win, text='Date')
+l1.grid(row=0,column=0)
+l2 = Label(win, text='Earnings')
+l2.grid(row=0,column=2)
+l3 = Label(win, text='Exercise')
+l3.grid(row=1,column=0)
+l4 = Label(win, text='Study')
+l4.grid(row=1,column=2)
+l5 = Label(win, text='Diet')
+l5.grid(row=2,column=0)
+l6 = Label(win, text='Python')
+l6.grid(row=2,column=2)
+
+date_text = StringVar()
+e1 = Entry(win, textvariable=date_text)
+e1.grid(row=0,column=1)
+
+earning_text = StringVar()
+e2 = Entry(win, textvariable=earning_text)
+e2.grid(row=0,column=3)
+
+exercise_text = StringVar()
+e3 = Entry(win, textvariable=exercise_text)
+e3.grid(row=1,column=1)
+
+study_text = StringVar()
+e4 = Entry(win, textvariable=study_text)
+e4.grid(row=1,column=3)
+
+diet_text = StringVar()
+e5 = Entry(win, textvariable=diet_text)
+e5.grid(row=2,column=1)
+
+python_text = StringVar()
+e6 = Entry(win, textvariable=python_text)
+e6.grid(row=2,column=3)
+
+list = Listbox(win,height=8,width=35)
+list.grid(row=3,column=0,rowspan=9,columnspan=2)
+
+sb = Scrollbar(win)
+sb.grid(row=3,column=2,rowspan=9)
+
+list.bind('<<ListboxSelect>>',get_selected_row)
+
+b1 = Button(win,text='ADD',width=12,pady=5,command=add_command)
+b1.grid(row=3,column=3)
+
+b2 = Button(win,text='Search',width=12,pady=5,command=search_command)
+b2.grid(row=4,column=3)
+
+b3 = Button(win,text='Delete date',width=12,pady=5,command=delete_command)
+b3.grid(row=5,column=3)
+
+b4 = Button(win,text='View all',width=12,pady=5,command=view_command)
+b4.grid(row=6,column=3)
+
+b5 = Button(win,text='Close',width=12,pady=5,command = win.destroy)
+b5.grid(row=7,column=3)
+
+win.mainloop()
+
+print(u'\u2705')
